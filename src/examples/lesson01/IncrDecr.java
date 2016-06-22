bde395e005eb33f4c99a2f3ab14a3208804d14ea
Eugene1992 <deyneko55@gmail.com>
2016-06-22 21:34:48 +0300

    Hw01 added

---
+++ src/examples/lesson01/IncrDecr.java
@@ -0,0 +1,15 @@
+package lesson01;
+
+/**
+ * Created by Student on 6/22/2016.
+ */
+public class IncrDecr {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i++);
+        System.out.println(++i);
+        System.out.println(--i);
+        System.out.println(i--);
+
+    }
+}
---
+++ src/examples/lesson01/Test.java
@@ -0,0 +1,51 @@
+package lesson01;
+
+public class Test {
+    public static void main(String[] args) {
+        byte a = 127;
+        short s = 127;
+        int i = 200000000;
+        byte b;
+
+        int i1 = 23;
+        int i2 = 12;
+
+        i2 = i2++ * 20;
+        i2 += 20;
+
+        int i3;
+        int i4;
+
+        int i5, i6 = 10, i7;
+
+        i7 = (i6 + a) / s;
+
+        long l = i1 + i2;
+
+        float h = 12.0f;
+
+        double d = 12;
+
+//        System.out.println(d);
+
+        long l2 = s / a;
+
+        boolean bool1 = 2 > 3;
+        boolean bool2 = false;
+
+        char c1 = 92;
+        char c2 = 'a';
+        char c3 = '\u2122';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        b = 20;
+
+        byte b4 = 10;
+
+        b = (byte)(b + 10);
+
+    }
+}
