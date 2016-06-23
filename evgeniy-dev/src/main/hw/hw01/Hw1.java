6b27cd64711512bf72d0ef1d551f9a46f8817b5e
Evgeniy <eugenio.divin@gmail.com>
2016-06-23 14:39:06 +0300

    Add Hw1 (Not fully done)

--- evgeniy-dev/src/main/hw/hw01/Hw1.java
+++ evgeniy-dev/src/main/hw/hw01/Hw1.java
@@ -1,22 +1,21 @@
 class Hw1 {
     public static void main(String[] args) {
 
-//        From 1 to 8
+//        From 1 to 5
 
         byte b = 20 + 10;
         short s = 200 - 10;
         int i = 100 / 2;
         long l = 300 * 2;
-
-
+//6
         byte b1 = 20;
         byte b2 = (byte) (b + b1);
         System.out.println(b2);
-
+//7
         short s1 = 10;
         byte b3 = (byte) ((s + s1) / b1);
         System.out.println(b3);
-
+//8
         int i1 = (int) ((s / s1) * (b / b1));
         System.out.println(i1);
 
