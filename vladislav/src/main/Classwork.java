79043236f68831b6b3b4d1983d0e0643dfbce541
VladOleynik <voleynik2016@gmail.com>
2016-06-25 02:33:51 +0300

    Задачку сделал, но не смог в system.out отобразить числа в обратном порядке

---
+++ vladislav/src/main/Classwork.java
@@ -0,0 +1,17 @@
+
+public class Classwork {
+    public static void main(String[] args) {
+
+
+        for (int k = 1000; k != 0; k = k/2) {
+            if (k % 2 == 1) {
+                System.out.print("1");
+            } else if (k % 2 == 0) {
+                System.out.print("0");
+            }
+        }
+    }
+}
+
+
+
