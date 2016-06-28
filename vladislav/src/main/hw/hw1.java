501a50a8d533ba80b0ac99f4b225d1071054453c
VladOleynik <voleynik2016@gmail.com>
2016-06-28 18:29:52 +0300

    HW add

---
+++ vladislav/src/main/hw/hw1.java
@@ -0,0 +1,85 @@
+
+public class hw1 {
+    public static void main(String[] args) {
+
+/*        byte a;
+        short b;
+        int c;
+        long d;*/
+
+/*        a = 3 + 2;
+        b = 17 - 12;
+        c = 20 / 5;
+        d = 2 * 3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);*/
+
+/*
+        byte e = 3;
+        byte f = 4;
+        byte g = (byte) (e + f);
+        System.out.println(g);
+*/
+
+/*        short a1 = 21;
+        short a2 = 3;
+        byte a3 = 4;
+        byte b1 = (byte) ((a1 + a2) / a3);
+        System.out.println(b1);*/
+
+/*        short c1 = 30;
+        short c2 = 10;
+        byte c3 = 15;
+        byte c4 = 3;
+        int c5 = (c1 / c2) * (c3 / c4);
+        System.out.println(c5);*/
+
+/*        byte d1 = 1;
+        byte d2 = 2;
+        byte d3 = 3;
+        d1 = d2;
+        d2 = d3;
+        d3 = d1;
+        System.out.println(d1);
+        System.out.println(d2);*/
+
+/*        int e1 = 2;
+        int e2 = 3;
+        e1 = (e1 + e2) - e1;
+        e2 = (e2 + e1) - e2;
+        int a5 = 1;
+        int b5 = 2;
+        a5 = a5 + b5 - (b5 = a5);
+        System.out.println(a5);
+        System.out.println(b5);*/
+
+        // bonus
+        int ai = 103;
+        int k1 = ai % 2;
+        int b4 = ai/2;
+        int b6 = b4 % 2 ;
+        int c7 = b4/2;
+        int c6 = c7 % 2 ;
+        int d4 = c7/2;
+        int d6 = d4 % 2 ;
+        int e4 = d4/2;
+        int e6 = e4 % 2 ;
+        int f4 = e4/2;
+        int f6 = f4 % 2 ;
+        int g4 = f4/2;
+        int g6 = g4 % 2 ;
+
+        System.out.print(g6 + " ");
+        System.out.print(f6 + " ");
+        System.out.print(e6 + " ");
+        System.out.print(d6 + " ");
+        System.out.print(c6 + " ");
+        System.out.print(b6 + " ");
+        System.out.print(k1 + " ");
+
+    }
+}
+
+
---
+++ vladislav/src/main/hw/hw2.java
@@ -0,0 +1,88 @@
+
+import java.util.Scanner;
+public class hw2 {
+    public static void main(String[] args) {
+// задача 2
+/*        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число");
+        int i = scan.nextInt();
+        long a =  i %2;
+        if (a == 1) {
+            System.out.println("Нечётное число");
+        }
+        if (a == 0) {
+            System.out.println("Чётное число");
+        }*/
+
+// задача 3
+/*
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите два числа и мы узнаем какое ближе к 10");
+        double i = scan.nextInt();
+        double e = scan.nextInt();
+        double a = 10;
+        double b = e + (i-e)/2;
+            if ( b < a ) {
+                System.out.println("Число " + i + " ближе к 10");
+            } else if ( b == a){
+                System.out.println("Оба числа на равном расстоянии к числу " + a);
+            } else {
+                System.out.println("Число " + e + " ближе к 10");
+            }
+*/
+
+
+// задача 4
+/*        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите три разных числа");
+        double i = scan.nextInt();
+        double e = scan.nextInt();
+        double f = scan.nextInt();
+            if (i > e & i > f) {
+                System.out.println("Число " + i + " максимальное");
+            }    else if (e > i & e > f) {
+                System.out.println("Число " + e + " максимальное");
+            }    else if (f > i & f > e) {
+                System.out.println("Число " + f + " максимальное");
+            }*/
+
+
+
+// задача 5
+/*        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите четыре числа");
+        double i = scan.nextInt();
+        double e = scan.nextInt();
+        double f = scan.nextInt();
+        double g = scan.nextInt();
+        if (i > e & i > f & i < g) {
+            System.out.println("Число " + i + " второе по величине");
+        } else if (i > e & i < f & i > g) {
+            System.out.println("Число " + i + " второе по величине");
+        } else if (i < e & i > f & i > g) {
+            System.out.println("Число " + i + " второе по величине");
+
+        }  if (e > i & e > f & e < g) {
+            System.out.println("Число " + e + " второе по величине");
+        } else if (e > i & e < f & e > g) {
+            System.out.println("Число " + e + " второе по величине");
+        } else if (i < e & i > f & i > g) {
+            System.out.println("Число " + e + " второе по величине");
+
+        }        if (f > e & f > i & f < g) {
+            System.out.println("Число " + f + " второе по величине");
+        } else if (f > e & f < i & f > g) {
+            System.out.println("Число " + f + " второе по величине");
+        } else if (f < e & f > i & f > g) {
+            System.out.println("Число " + f + " второе по величине");
+
+        }        if (g > e & g > f & g < i) {
+            System.out.println("Число " + g + " второе по величине");
+        } else if (g > e & g < f & g > i) {
+            System.out.println("Число " + g + " второе по величине");
+        } else if (g < e & g > f & g > i) {
+            System.out.println("Число " + g + " второе по величине");
+        }*/
+
+    }
+}
