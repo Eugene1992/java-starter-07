a48604e957f7fd5b7fce2d15478b4fd9d3a0e5d6
EvgenVoronetskiy <evoronetskiy@gmail.com>
2016-06-30 18:57:18 +0300

    Home Work

--- evgeniy-vor/src/main/hw/hw04/Task.java
+++ evgeniy-vor/src/main/hw/hw04/Task.java
@@ -9,10 +9,10 @@ public class Task {
         //слева от двоеточия показывается симметричная комбинация для той,
         // что справа от двоеточия (например, 02:20, 11:11 или 15:51).
 
-      //  String time ="";
+        //  String time ="";
         String hours = "";
         String minutes = "";
-      //  String minutesRevers = "";
+        //  String minutesRevers = "";
         int count = 0;
 
         for(int i = 0; i <= 23; i++){
@@ -21,23 +21,35 @@ public class Task {
 
                 if(i < 10){
                     if(j < 10){
-                       // System.out.println("0" + i + ":" + "0" + j);
-                    //    time = "0" + i + ":" + "0" + j;
+                        // System.out.println("0" + i + ":" + "0" + j);
+                        //    time = "0" + i + ":" + "0" + j;
                         hours = "0" + i;
                         minutes = "0" + j;
 
                     }else{
-                      //  System.out.println("0" + i + ":" + j);
-                    //    time = "0" + i + ":" + j;
+                        //  System.out.println("0" + i + ":" + j);
+                        //    time = "0" + i + ":" + j;
                         hours = "0" + i;
                         minutes = j + "";
 
                     }
                 }else{
-                   // System.out.println(i + ":" + j);
-                 //   time = i + ":" + j;
+                    // System.out.println(i + ":" + j);
+                    //   time = i + ":" + j;
                     hours = i +"";
-                    minutes = j + "";
+
+                    if(j < 10){
+                        // System.out.println("0" + i + ":" + "0" + j);
+                        //    time = "0" + i + ":" + "0" + j;
+
+                        minutes = "0" + j;
+
+                    }else{
+                        //  System.out.println("0" + i + ":" + j);
+                        //    time = "0" + i + ":" + j;
+
+                        minutes = j + "";
+                    }
                 }
 
                 for(int k = 0; k < minutes.length(); k++){
@@ -45,11 +57,12 @@ public class Task {
                 }
                 if(hours.equals(minutesRevers)){
                     count ++;
+                    //      System.out.println("ResultTime = " + hours + ":" + minutes);
                 }
 
-                System.out.println(hours + ":" + minutes);
-            //    System.out.println(hours + ":" + minutesRevers);
-                System.out.println();
+                //    System.out.println(hours + ":" + minutes);
+                //    System.out.println(hours + ":" + minutesRevers);
+                //    System.out.println();
             }
         }
 
